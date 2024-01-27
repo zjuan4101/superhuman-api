81fb9a5eba2ad14642d1cb51a4123816acb61e30
Juan Zelaya Castillo <zjuan4101@gmail.com>
2024-01-27 10:59:18 -0500

    creted the superhumans controller

---
+++ controllers/superhumansController.js
@@ -0,0 +1,76 @@
+const Superhuman = require('../models/superhuman')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+// create
+const create = async (req, res) => {
+    const { name, alias, power, weakness, isHero } = req.body
+
+    try {
+        const newSuperhuman = new Superhuman({ name, alias, power, weakness, isHero })
+        const savedSuperhuman = await newSuperhuman.save()
+        res.status(200).json(savedSuperhuman)
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+}
+
+// index
+const index = async (req, res) => {
+    try {
+        const superhumans = await Superhuman.find()
+        res.json(superhumans)
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+}
+
+// show
+const show = async (req, res) => {
+    const id = req.params.id
+    try {
+        const superhuman = await Superhuman.findOne({ _id: id })
+        if(!superhuman) {
+            res.status(404).json({ message: 'Superhuman not found' })
+            retrun
+        }
+
+        res.json(superhuman)
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+}
+
+// update
+const update = async (req, res) => {
+    const id = req.params.id
+    const updates = req.body
+
+    try {
+        const updatedSuperhuman = await Superhuman.findOneAndUpdate({ _id: id}, updates, { new: true })
+        if(!updatedSuperhuman) {
+            res.status(404).json({ message: 'Superhuman not found' })
+            return
+        }
+
+        res.json(updatedSuperhuman)
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+}
+
+// delete
+const destroy = async (req, res) => {
+    const id = req.params.id
+
+    try {
+        const deletedSuperhuman = await Superhuman.findOneAndDelete({ _id: id })
+        if(!deletedSuperhuman) {
+            res.status(404).json({ message: 'Superhuman not found' })
+            retrun
+        }
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+}
+
